27edc7cfdab517c2ad12a3e3b4203111f4a3209e
Jonas Kell <jonas-kell@web.de>
2025-11-07 12:06:30 +0100

    feat: window!

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,53 @@
 use crate::args::Args;
+use winit::{
+    dpi::{PhysicalPosition, PhysicalSize},
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
-/// Listen on the UDP Port for the client and poll the server for packets to transfer
 pub async fn run_client(args: &Args) -> () {
     let _ = args;
-    todo!("Not implemented");
+
+    let event_loop = EventLoop::new();
+
+    // Desired size (pixels) and position (pixels from top-left of screen)
+    let width = 640u32;
+    let height = 360u32;
+    let pos_x = 800i32;
+    let pos_y = 400i32;
+
+    let window = WindowBuilder::new()
+        .with_title("Display Window")
+        // initial inner size
+        .with_inner_size(PhysicalSize::new(width, height))
+        // prevent user resizing by setting min and max to same size
+        .with_min_inner_size(PhysicalSize::new(width, height))
+        .with_max_inner_size(PhysicalSize::new(width, height))
+        .with_decorations(false)
+        .build(&event_loop)
+        .unwrap();
+
+    // Set outer position (some window managers may ignore or alter this)
+    window.set_outer_position(PhysicalPosition::new(pos_x, pos_y));
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    error!("Tried to close Window -> Cant allow that");
+                }
+                WindowEvent::Resized(_) | WindowEvent::ScaleFactorChanged { .. } => {
+                    // Could re-scale windo here. Probably not necessary though
+                }
+                _ => {}
+            },
+            Event::MainEventsCleared => {
+                // place any drawing / UI update calls here (none for an empty window)
+            }
+            _ => {}
+        }
+    });
 }
