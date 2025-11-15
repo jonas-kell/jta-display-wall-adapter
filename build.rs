c18de22225709a9954492acb7029f69f65f964c3
Jonas Kell <jonas-kell@web.de>
2025-11-15 17:08:32 +0100

    feat: make sure it is rebuilding

--- build.rs
+++ build.rs
@@ -7,12 +7,14 @@ fn main() {
     println!("cargo:rerun-if-changed=images_core/");
     println!("cargo:rerun-if-changed=assets/");
 
+    println!("cargo:warning=Building image cache now!");
     let sizes_to_pre_cache = [(360, 120)];
     let storage = ImagesStorage::new_with_compile_data(&sizes_to_pre_cache);
+    println!("cargo:warning=Files loaded");
     let bytes = storage.to_bytes();
-
+    println!("cargo:warning=Converted to bytes");
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let dest = PathBuf::from(out_dir).join("image_storage.bin");
-
     fs::write(&dest, bytes).unwrap();
+    println!("cargo:warning=Written for consumption");
 }
