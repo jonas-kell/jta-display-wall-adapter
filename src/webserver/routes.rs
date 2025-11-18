699a9467152cc78ee6f10028275253dd475d9237
Jonas Kell <jonas-kell@web.de>
2025-11-19 00:24:14 +0100

    feat: websocket heartbeat and reconnection

--- src/webserver/routes.rs
+++ src/webserver/routes.rs
@@ -1,9 +1,23 @@
 use actix_web::{web, HttpRequest, Responder};
 use actix_ws::Message;
 use futures::StreamExt;
+use std::time::Duration;
 
 pub async fn ws_route(req: HttpRequest, body: web::Payload) -> actix_web::Result<impl Responder> {
     let (response, mut session, mut msg_stream) = actix_ws::handle(&req, body)?;
+
+    // HEARTBEAT TASK
+    let mut hb_session = session.clone();
+    actix_web::rt::spawn(async move {
+        loop {
+            actix_web::rt::time::sleep(Duration::from_secs(5)).await;
+
+            // send ping or stop if connection closed
+            if hb_session.ping(b"").await.is_err() {
+                break;
+            }
+        }
+    });
 
     actix_web::rt::spawn(async move {
         while let Some(Ok(msg)) = msg_stream.next().await {
@@ -12,6 +26,9 @@ pub async fn ws_route(req: HttpRequest, body: web::Payload) -> actix_web::Result
                     if session.pong(&bytes).await.is_err() {
                         return;
                     }
+                }
+                Message::Pong(_) => {
+                    trace!("Websocket received pong");
                 }
                 Message::Text(msg) => println!("Got text: {msg}"),
                 _ => break,
--- web_client/src/stores/main.ts
+++ web_client/src/stores/main.ts
@@ -2,15 +2,64 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { WS_URL } from "../functions/environment";
 
+function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export default defineStore("main", () => {
     const connected = ref(false);
+    let reconnecting = false;
 
-    let ws = new WebSocket(WS_URL);
+    let ws = null as null | WebSocket;
 
-    ws.onmessage = (ev) => console.log(ev.data);
+    function handleWSMessage(ev: any) {
+        console.log(ev.data);
+    }
+
+    async function initWS() {
+        if (reconnecting) return; // prevent multiple runs
+        reconnecting = true;
+        console.log("Reconnecting WS");
+
+        if (ws) {
+            try {
+                ws.close();
+            } catch (_) {}
+            ws = null;
+            await sleep(500);
+        }
+
+        ws = new WebSocket(WS_URL);
+        ws.onclose = () => {
+            connected.value = false;
+            initWS();
+        };
+        ws.onerror = async () => {
+            connected.value = false;
+            initWS();
+        };
+        ws.onmessage = handleWSMessage;
+        ws.onopen = () => {
+            connected.value = true;
+            reconnecting = false;
+        };
+    }
+    initWS();
+
+    function sendWSCommand(dat: string) {
+        if (ws && connected.value) {
+            ws.send(dat);
+        }
+        if (!ws) {
+            console.error("Websocket was never connected");
+        }
+        if (!connected.value) {
+            console.error("Websocket is not connected");
+        }
+    }
 
     function sendAdvertisementsCommand() {
-        ws.send("TEST");
+        sendWSCommand("Test");
     }
 
     return { connected, sendAdvertisementsCommand };
