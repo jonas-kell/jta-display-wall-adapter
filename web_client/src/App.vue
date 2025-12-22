00df766107be1ce86ca51e099ecba4d56e752633
Jonas Kell <jonas-kell@web.de>
2025-12-22 12:49:29 +0100

    feat: make db init page prettier

--- web_client/src/App.vue
+++ web_client/src/App.vue
@@ -30,35 +30,48 @@
 </script>
 
 <template>
-    <template v-if="!mainStore.connected && !connectedOnce">
-        <h1>JTA Display Wall Adapter</h1>
-        Connecting....
-    </template>
-    <template v-else>
-        <template v-if="mainStore.staticConfiguration == null">
-            <p>Server not Configured!!</p>
-            <br />
-            Date this database will be used:
-            <br />
-            <input type="date" v-model="appDate" />
-
-            <br />
-            <br />
-
-            Mode:
-            <br />
-            <select v-model="modeSelect" style="min-width: 100px">
-                <option :value="ApplicationMode.TrackCompetition">Normal Track Competition</option>
-                <option :value="ApplicationMode.StreetLongRun">Street Long Run</option>
-                <option :value="ApplicationMode.SprinterKing">Sprinter König</option>
-            </select>
-
-            <br />
-            <br />
-            <button @click="configure">Configure!</button>
+    <div class="ma-2">
+        <template v-if="!mainStore.connected && !connectedOnce">
+            <h1>JTA Display Wall Adapter</h1>
+            Connecting....
         </template>
-        <RouterView v-else />
-    </template>
+        <template v-else>
+            <template v-if="mainStore.staticConfiguration == null">
+                <h1>JTA Display Wall Adapter</h1>
+                <div class="ma-5">
+                    <p class="mb-5">Server not Configured!!</p>
+
+                    <v-text-field type="date" v-model="appDate" density="compact" label="Date this database will be used" />
+                    <v-select
+                        v-model="modeSelect"
+                        density="compact"
+                        label="Mode"
+                        item-title="label"
+                        item-value="value"
+                        :items="[
+                            {
+                                label: 'Normal Track Competition',
+                                value: ApplicationMode.TrackCompetition,
+                            },
+                            {
+                                label: 'Street Long Run',
+                                value: ApplicationMode.StreetLongRun,
+                            },
+                            {
+                                label: 'Sprinter König',
+                                value: ApplicationMode.SprinterKing,
+                            },
+                        ]"
+                    >
+                    </v-select>
+
+                    <br />
+                    <v-btn @click="configure">Configure!</v-btn>
+                </div>
+            </template>
+            <RouterView v-else />
+        </template>
+    </div>
 </template>
 
 <style scoped></style>
